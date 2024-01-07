b69812f68778cba3aef0b03e075bf93728b04290
Rupdeep Dey <113031805+rupdeep2275@users.noreply.github.com>
2024-01-07 15:02:58 +0530

    added test case

--- src/test/java/com/example/userservice/InsertRegisteredClientTest.java
+++ src/test/java/com/example/userservice/InsertRegisteredClientTest.java
@@ -13,28 +13,28 @@ import org.springframework.security.oauth2.server.authorization.settings.ClientS
 
 import java.util.UUID;
 
-@SpringBootTest
+//@SpringBootTest
 public class InsertRegisteredClientTest {
-    @Autowired
+//    @Autowired
     private PasswordEncoder bCryptPasswordEncoder;
 
-    @Autowired
+//    @Autowired
     private JpaRegisteredClientRepository jpaRegisteredClientRepository;
 
-    @Test
+//    @Test
     public void insertNewClientToDb(){
-        RegisteredClient productClient = RegisteredClient.withId(UUID.randomUUID().toString())
-                .clientId("productservice")
-                .clientSecret(bCryptPasswordEncoder.encode("productservicepassword"))
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-                .redirectUri("https://oauth.pstmn.io/v1/callback")
-                .postLogoutRedirectUri("http://127.0.0.1:8080/")
-                .scope(OidcScopes.OPENID)
-                .scope(OidcScopes.PROFILE)
-                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
-                .build();
-        jpaRegisteredClientRepository.save(productClient);
+//        RegisteredClient productClient = RegisteredClient.withId(UUID.randomUUID().toString())
+//                .clientId("productservice")
+//                .clientSecret(bCryptPasswordEncoder.encode("productservicepassword"))
+//                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+//                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+//                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+//                .redirectUri("https://oauth.pstmn.io/v1/callback")
+//                .postLogoutRedirectUri("http://127.0.0.1:8080/")
+//                .scope(OidcScopes.OPENID)
+//                .scope(OidcScopes.PROFILE)
+//                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+//                .build();
+//        jpaRegisteredClientRepository.save(productClient);
     }
 }
